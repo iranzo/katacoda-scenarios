47fb619d1d68c0f0724c2db2c55665187fc9705b
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2019-08-14 19:00:11 +0200

    Clean clutter and invite to proceed

--- kubevirt-upgrades/set-env.sh
+++ kubevirt-upgrades/set-env.sh
@@ -6,3 +6,6 @@ export KUBEVIRT_LATEST_VERSION=$(curl -s https://api.github.com/repos/kubevirt/k
 # Download virctl
 wget -O virtctl https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_LATEST_VERSION}/virtctl-${KUBEVIRT_LATEST_VERSION}-linux-amd64
 chmod +x virtctl
+clear
+
+echo "Environment is ready and virtctl is installed, go ahead"
