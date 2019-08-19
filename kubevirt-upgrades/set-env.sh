7a18ee96375f9f64d2928c2dffe54f86dfd1a7f9
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2019-08-19 09:39:56 +0200

    Hide progress

--- kubevirt-upgrades/set-env.sh
+++ kubevirt-upgrades/set-env.sh
@@ -1,10 +1,10 @@
-launch.sh
+launch.sh > /dev/null 2>&1
 
 # Get lateste KubeVirt virtctl
-export KUBEVIRT_LATEST_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases/latest | jq -r .tag_name)
+export KUBEVIRT_LATEST_VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases/latest | jq -r .tag_name) > /dev/null 2>&1
 
 # Download virctl
-wget -O virtctl https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_LATEST_VERSION}/virtctl-${KUBEVIRT_LATEST_VERSION}-linux-amd64
+wget -O virtctl https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_LATEST_VERSION}/virtctl-${KUBEVIRT_LATEST_VERSION}-linux-amd64 > /dev/null 2>&1
 chmod +x virtctl
 clear
 
